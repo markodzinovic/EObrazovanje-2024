fbda313328feb73867c59539adda8fc5c9888f58
DESKTOP-HJTAGP2\Korisnik <dragoljubbrkic96@gmail.com>
2024-10-25 21:25:27 +0200

    Added back and front project

---
+++ sluzba/src/test/java/com/ftn/sluzba/SluzbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.sluzba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SluzbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
