a24383fa3d9b1331f1e1d656163fc8f90955abf6
DESKTOP-HJTAGP2\Korisnik <dragoljubbrkic96@gmail.com>
2024-11-05 23:26:13 +0100

    Enabled cors on back

--- sluzba/src/main/java/com/ftn/sluzba/security/SecurityConfig.java
+++ sluzba/src/main/java/com/ftn/sluzba/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.ftn.sluzba.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -32,13 +38,25 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		 CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Specify allowed origins
+	        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow all methods
+	        configuration.setAllowedHeaders(List.of("*")); // Allow all headers
+	        configuration.setAllowCredentials(true); // Allow credentials
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration); // Apply to all paths
+	        return source;
+	}
+
+	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
+		http.cors(withDefaults()) // Ensure 'and()' is called to continue the configuration
+				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/index.html", "/api/test", "/api/register", "/api/login")
-								.permitAll().requestMatchers(HttpMethod.POST, "/api/**").hasAuthority("ROLE_ADMIN")
-								.anyRequest().authenticated());
+								.permitAll().anyRequest().authenticated());
 
 		http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
